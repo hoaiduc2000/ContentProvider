ab3c4780f73a1dd40c1089a1d5d77728707e101d
Hoai Duc <hoaiduc2000@gmail.com>
2016-08-18 17:33:52 +0700

    Initial commit

---
+++ app/src/main/java/nguyenhoaiduc/contentprovider/MainActivity.java
@@ -0,0 +1,15 @@
+package nguyenhoaiduc.contentprovider;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by nguyen.hoai.duc on 8/18/2016.
+ */
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+    }
+}
